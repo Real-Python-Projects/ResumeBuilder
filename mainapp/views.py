dbee3f20860346d925342101e34bc912d6d69ae6
Gateway47 <itsregalo047@gmail.com>
2021-03-31 12:12:35 +0300

    created user resume create view

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import (
     PersonalData, CareerObjective, EducationBackground,
     OtherQualification, Employment, Skills,
@@ -13,6 +13,156 @@ from django.template.loader import render_to_string
 
 def IndexView(request, *args, **kwargs):
     return render(request, 'index.html')
+
+def CreatePDataView(request, *args, **kwargs):
+    if request.method == 'POST':
+        user = request.user
+        resume = UserResume.objects.get(user=user)
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        nationality = request.POST['nationality']
+        id_no = request.POST['id_no']
+        address_code = request.POST['address_code']
+        address_box = request.POST['address_box']
+        tel_no = request.POST['tel_no']
+        email = request.POST['email']
+        
+        pData = PersonalData.objects.create(
+            user = user,
+            resume = resume,
+            first_name = first_name,
+            last_name = last_name,
+            nationality = nationality,
+            id_no = id_no,
+            address_code = address_code,
+            address_box = address_box,
+            tel_no = tel_no,
+            email = email
+        )
+        pData.save()
+        return HttpResponseRedirect()
+    return render(request, 'personal_data.html')
+
+def CreateCareerObjectiveView(request, *args, **kwargs):
+    if request.method == 'POST':
+        user = request.user
+        resume = UserResume.objects.get(user=user)
+        content = request.POST['content']     
+        carObjective = CareerObjective.objects.create(
+            user = user,
+            resume = resume,
+            content = content
+        )
+        carObjective.save()
+        return HttpResponseRedirect()
+    return render(request, 'career_objective.html')
+
+def CreateEducationBackgroundView(request, *args, **kwargs):
+    if request.method == 'POST':
+        start_date = request.POST['start_date']
+        done_date = request.POST['done_date'] or None
+        institution = request.POST['institution']
+        
+        edBackground = EducationBackground.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            start_date = start_date,
+            done_date = done_date,
+            institution = institution   
+        )
+        edBackground.save()
+        return HttpResponseRedirect()
+    return render(request, 'edu-background-form.html')
+
+def CreateOtherQualificationView(request, *args, **kwargs):
+    if request.method == 'POST':
+        start_date = request.POST['start_date']
+        done_date = request.POST['done_date'] or None
+        institution = request.POST['institution']
+        
+        edBackground = EducationBackground.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            start_date = start_date,
+            done_date = done_date,
+            institution = institution   
+        )
+        edBackground.save()
+        return HttpResponseRedirect()
+    return render(request, 'other-qualification-form.html')
+
+def CreateEmloymentView(request, *args, **kwargs):
+    if request.method == 'POST':
+        start_date = request.POST['start_date']
+        done_date = request.POST['done_date'] or None
+        institution = request.POST['institution']
+        responsibility = request.POST['responsibility']
+        duties = request.POST['duties']
+        
+        emp = Employment.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            start_date = start_date,
+            done_date = done_date,
+            institution = institution,
+            responsibility = responsibility,
+            duties = duties   
+        )
+        emp.save()
+        return HttpResponseRedirect()
+    return render(request, 'emp-history-form.html')
+
+def CreateSkillsView(request, *args, **kwargs):
+    if request.method == 'POST':
+        skill = request.POST['skill']
+        
+        skillDb = Skills.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            skill = skill
+        )
+        skillDb.save()
+        return HttpResponseRedirect()
+    return render(request, 'skills-form.html')
+
+def CreateInterestView(request, *args, **kwargs):
+    if request.method == 'POST':
+        interest = request.POST['interest']
+        
+        interestDb = Skills.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            interest = interest
+        )
+        interestDb.save()
+        return HttpResponseRedirect()
+    return render(request, 'interest-form.html')
+
+def CreateReferenceView(request, *args, **kwargs):
+    if request.method == 'POST':
+        title = request.POST['title'],
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        work_title = request.POST['work_title']
+        company = request.POST['company']
+        tel_no = request.POST['tel_no']
+        email = request.POST['email']
+        town = request.POST['town']
+        ref = References.objects.create(
+            user = request.user,
+            resume = UserResume.objects.get(user=request.user),
+            title = title,
+            first_name = first_name,
+            last_name = last_name,
+            work_title = work_title,
+            company = company,
+            tel_no = tel_no,
+            email = email,
+            town = town
+        )
+        ref.save()
+        return HttpResponseRedirect()
+    return render(request, 'references-form.html')
 
 def GeneratePdf(request):
     resume = get_object_or_404(UserResume, user=request.user)
